134a7aaa8f0a7f25fedef629f6f2e20583337883
Vickybel <vickybel1995@hotmail.com>
2020-09-09 22:21:33 +0000

    login

--- front/src/js/store/flux.js
+++ front/src/js/store/flux.js
@@ -1,97 +1,8 @@
-/* const getState = ({ getStore, getActions, setStore }) => {
- return {
-  store: {
-   url_magazine: "https://5000-b8b51ae6-b147-4b20-bf2b-061f1de363ad.ws-us02.gitpod.io/magazine",
-   magazines: [],
-   banners: [],
-   carousels: [],
-   magazine_name: null,
-   magazine_date: null,
-   magazine_glance: null,
-   magazine_body: null
-  },
-  actions: {
-   getMagazine: () => {
-    fetch("https://5000-e415c755-f0d4-4272-8c20-df2b48e34360.ws-us02.gitpod.io/premium");
-    const store = getStore();
-    fetch("https://5000-c9c6b521-788f-42eb-ae49-fadcea8f2c1a.ws-us02.gitpod.io/magazine")
-     .then(res => res.json())
-     .then(data => {
-      setStore({ magazines: data });
-     });
-   },
-   handleChange: e => {
-    const { name, value } = e.target;
-    setStore({
-     [name]: value[0]
-    });
-   },
-   handleFile: e => {
-    const { name, files } = e.target;
-    setStore({
-     [name]: files[0]
-    });
-   },
-   handleUpdateMagazine: e => {
-    e.preventDefault();
-    const store = getStore();
-
-    let formData = new FormData();
-    formData.append("glance", store.magazine_glance);
-    formData.append("name", store.magazine_name);
-    formData.append("date", store.magazine_date);
-
-    //formData.append("name", store.name);
-    //formData.append("lastname", store.lastname);
-
-    // fetch(`${store.apiUrl}/update-profile`, {
-
-    fetch("https://5000-c9c6b521-788f-42eb-ae49-fadcea8f2c1a.ws-us02.gitpod.io/magazine", {
-     method: "POST",
-     body: formData,
-     headers: {
-      // Authorization: Bearer ${store.currentUser.access_token}
-     }
-    })
-     .then(resp => resp.json())
-     .then(data => {
-      console.log(data);
-      // let {currentUser} = store;
-      // currentUser['user'] = data.user;
-      setStore({
-       // success: data.success,
-       // currentUser: currentUser,
-       magazine_glance: null
-      });
-     });
-   },
-   getBanner: () => {
-    //fetch("https://5000-ddd8b02e-6351-44cc-82b7-00ec1e966a70.ws-us02.gitpod.io/banner")
-    fetch("https://5000-e415c755-f0d4-4272-8c20-df2b48e34360.ws-us02.gitpod.io/premium")
-     .then(res => res.json())
-     .then(data => {
-      setStore({ banners: data });
-     });
-   },
-   getCarousel: () => {
-    //fetch("https://5000-ddd8b02e-6351-44cc-82b7-00ec1e966a70.ws-us02.gitpod.io/carousel")
-    fetch("https://5000-e415c755-f0d4-4272-8c20-df2b48e34360.ws-us02.gitpod.io/premium")
-     .then(res => res.json())
-     .then(data => {
-      setStore({ carousels: data });
-     });
-   }
-  }
- };
-}; */
-
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
-			apiUrl: "https://5000-c52f4e43-f7de-4e76-8ef8-5e8d0cd01d39.ws-us02.gitpod.io",
+			apiUrl: "https://5000-f640acd6-33c8-4560-a61e-674066f61a41.ws-us02.gitpod.io/",
 			magazine: {
-				apiUrl: "https://5000-c9c6b521-788f-42eb-ae49-fadcea8f2c1a.ws-us02.gitpod.io/magazine",
-				url: "https://5000-b4add0ee-14ad-4ce9-be91-b858ddcfbf50.ws-us02.gitpod.io",
 				magazines: [],
 				magazine_name: "",
 				magazine_date: "",
@@ -208,7 +119,7 @@ const getState = ({ getStore, setStore }) => {
 
 			deleteMagazine: e => {
 				const store = getStore();
-				fetch(`https://5000-d71b49a7-8903-44db-ae1a-99aa5de996c2.ws-us02.gitpod.io/magazine${e.target.id}`, {
+				fetch(`https://5000-b5e73340-cac7-42f0-adf0-db1bf1692c22.ws-us02.gitpod.io/magazine${e.target.id}`, {
 					method: "DELETE",
 					headers: {
 						"Content-Type": "application/json"
@@ -238,7 +149,7 @@ const getState = ({ getStore, setStore }) => {
 			updateMagazine: (e, idform) => {
 				e.preventDefault();
 				const store = getStore();
-				fetch(`https://5000-d71b49a7-8903-44db-ae1a-99aa5de996c2.ws-us02.gitpod.io/magazine/${e.target.id}`, {
+				fetch(`https://5000-b5e73340-cac7-42f0-adf0-db1bf1692c22.ws-us02.gitpod.io/${e.target.id}`, {
 					method: "PUT",
 					body: JSON.stringify(store.magazine),
 					headers: {
@@ -266,15 +177,12 @@ const getState = ({ getStore, setStore }) => {
 			},
 
 			GetMagazineToUpdate: e => {
-				fetch(
-					`https://5000-d71b49a7-8903-44db-ae1a-99aa5de996c2.ws-us02.gitpod.io/magazine/${e.match.params.id}`,
-					{
-						method: "GET",
-						headers: {
-							"Content-Type": "application/json"
-						}
+				fetch(`https://5000-b5e73340-cac7-42f0-adf0-db1bf1692c22.ws-us02.gitpod.io/${e.match.params.id}`, {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json"
 					}
-				)
+				})
 					.then(resp => {
 						return resp.json();
 					})
